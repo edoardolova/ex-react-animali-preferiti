9fd62d685574a3d4fb3f2a016b8a7a3e90f4622d
Edoardo Lova <edoardolova81@gmail.com>
2025-10-24 12:25:49 +0200

    feat: milestone 2, add random animal

--- reactApp.js
+++ reactApp.js
@@ -1,7 +1,20 @@
 const ReactApp = () =>{
-    const animals = ['gatto', 'cane', 'topo', 'scimmia', 'capra', 'serpente']
+    const {useState} = React;
+    const animalsChoices = ["Cane", "Gatto", "Pappagallo", "Cavallo", "Panda"];
+    const [animals, setAnimals] = useState([])
+
+    function addRandomAnimal(){
+        const rand = Math.floor(Math.random()*animalsChoices.length)
+        const animalToAdd = animalsChoices[rand];
+        console.log(animalToAdd, rand)
+        const updatedAnimals =[...animals, animalToAdd]
+        setAnimals(updatedAnimals)
+
+    }
+
     return(
         <section>
+            <button onClick={addRandomAnimal}>Agiungi Animale</button>
             <details>
                 <summary>Animali</summary>
                 <ul>
